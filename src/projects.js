b676424bd41f8737596dfab9b24f64bbeb8809be
J cao <juliankamilo6@gmail.com>
2024-11-16 15:26:53 -0500

    images path

--- src/projects.js
+++ src/projects.js
@@ -1,54 +1,55 @@
 export const projects1 = [
-  {
-    id: 1,
-    name: "TodoList",
-    description: "Aplicacion para la gestion de tareas",
-    languaje: "React",
-    images: [
-      "/public/images/react.png",
-      "/public/images/reactToDo/modal.png",
-      "/public/images/reactToDo/start.png",
-      "/public/images/reactToDo/todo.png",
-    ],
-    color:""
-  },
-  {
-    id: 2,
-    name: "Codink Game",
-    description: "Juego de adivinar el codigo ",
-    languaje: "Vue",
-    images: [
-      "/public/images/react.png",
-      "/public/images/reactToDo/modal.png",
-      "/public/images/reactToDo/start.png",
-      "/public/images/reactToDo/todo.png",
-    ],
-    color:""
-  },
-  {
-    id: 3,
-    name: "Pygame",
-    description: "Juego en la libreria de python pygame",
-    languaje: "Python",
-    images: [
-      "/public/images/react.png",
-      "/public/images/reactToDo/modal.png",
-      "/public/images/reactToDo/start.png",
-      "/public/images/reactToDo/todo.png",
-    ],
-    color:""
-  },
-  {
-    id: 4,
-    name: "Java",
-    description: "Trabajando estaciones de bus en la consola",
-    languaje: "Java",
-    images: [
-      "/public/images/react.png",
-      "/public/images/reactToDo/modal.png",
-      "/public/images/reactToDo/start.png",
-      "/public/images/reactToDo/todo.png",
-    ],
-    color:""
-  },
-];
+    {
+      id: 1,
+      name: "TodoList",
+      description: "Aplicacion para la gestion de tareas",
+      languaje: "React",
+      images: [
+        "/images/react.png",
+        "/images/reactToDo/modal.png",
+        "/images/reactToDo/start.png",
+        "/images/reactToDo/todo.png",
+      ],
+      color: "",
+    },
+    {
+      id: 2,
+      name: "Codink Game",
+      description: "Juego de adivinar el codigo",
+      languaje: "Vue",
+      images: [
+        "/images/react.png",
+        "/images/reactToDo/modal.png",
+        "/images/reactToDo/start.png",
+        "/images/reactToDo/todo.png",
+      ],
+      color: "",
+    },
+    {
+      id: 3,
+      name: "Pygame",
+      description: "Juego en la libreria de python pygame",
+      languaje: "Python",
+      images: [
+        "/images/react.png",
+        "/images/reactToDo/modal.png",
+        "/images/reactToDo/start.png",
+        "/images/reactToDo/todo.png",
+      ],
+      color: "",
+    },
+    {
+      id: 4,
+      name: "Java",
+      description: "Trabajando estaciones de bus en la consola",
+      languaje: "Java",
+      images: [
+        "/images/react.png",
+        "/images/reactToDo/modal.png",
+        "/images/reactToDo/start.png",
+        "/images/reactToDo/todo.png",
+      ],
+      color: "",
+    },
+  ];
+  
